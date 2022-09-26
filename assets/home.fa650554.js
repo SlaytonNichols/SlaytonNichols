7a4ee95938ab79402f5f6093979a813ee7ac2ecd
GitHub Action <action@github.com>
2022-09-26 22:47:07 +0000

    deploy: 2ee61b5065c7277618965a69f23b59b828f48d79

--- assets/home.fa650554.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as o,b as t}from"./app.5fabd02b.js";const n={preserveAspectRatio:"xMidYMid meet",viewBox:"0 0 24 24",width:"1.2em",height:"1.2em"},c=t("path",{fill:"currentColor",d:"M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8h5Z"},null,-1),s=[c];function h(i,r){return e(),o("svg",n,s)}const a={name:"mdi-home",render:h};export{a as _};
