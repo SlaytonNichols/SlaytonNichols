634953e9445365c803a6a3eb1795b56c7b6b4a68
Slayton Nichols <nicholsslayton@gmail.com>
2022-10-23 12:57:54 -0400

    feat(*): add datadog rum

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useApp } from "@/api"
+import { datadogRum } from '@datadog/browser-rum';
 
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
@@ -14,6 +15,23 @@ useHead({
 if (!import.meta.env.SSR) {
   let store = useApp()
   store.load()
+  datadogRum.init({
+    applicationId: '6f4b27ea-61b6-4676-a1c0-96492230da6e',
+    clientToken: 'pubd496da5f7fd8ca374c30db223be9a4a9',
+    site: 'us3.datadoghq.com',
+    service:'slaytonnichols',
+    
+    // Specify a version number to identify the deployed version of your application in Datadog 
+    // version: '1.0.0',
+    sampleRate: 100,
+    sessionReplaySampleRate: 20,
+    trackInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel:'mask-user-input'
+});
+    
+datadogRum.startSessionReplayRecording();
 }
 </script>
 
